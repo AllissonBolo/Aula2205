a13ff4de1e58fd166556efe9205b8437c426699b
Rafaela <rafaelam.santana01@gmail.com>
2026-05-22 12:09:39 -0300

    Arquivos atualizados

--- app/src/main/java/br/sp/etec/sebrae/BoletoActivity.kt
+++ app/src/main/java/br/sp/etec/sebrae/BoletoActivity.kt
@@ -1,28 +1,20 @@
 package br.sp.etec.sebrae
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class HomeActivity : AppCompatActivity() {
+class BoletoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_boleto)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        var btnBoleto = findViewById<Button>(R.id.btnBoleto)
-        btnBoleto.setOnClickListener {
-            var intent = Intent(this, BoletoActivity::class.java)
-            startActivity(intent)
         }
     }
 }
